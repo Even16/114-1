4eb11180f27b138490aab6baa1e6455a4fa1eaeb
Even16 <sonne0315@gmail.com>
2026-01-11 20:07:14 +0800

    0111
    0111

--- Assets/scripts/Find NPC.cs
+++ Assets/scripts/Find NPC.cs
@@ -9,6 +9,7 @@ public class FindNPC : MonoBehaviour
     GameObject 目標敵人 = null;
     GameObject[] 所有敵人;
     public GameObject 玩家;
+    public Vector3 原始瞄準點 = new Vector3(0.027f,1.187f,0.964f);
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -36,12 +37,15 @@ public class FindNPC : MonoBehaviour
                 目標敵人 = 敵人;
             }
         }
-        if (最短距離 > 最長距離)
-        {
-            目標敵人 = null;
-        }
         Vector3 瞄準位置 = 目標敵人.transform.position;
         瞄準位置.y = 1.5f;
         this.transform.position = 目標敵人.transform.position;
+        if (最短距離 > 最長距離)
+        {
+            目標敵人 = null;
+            this.transform.position = 原始瞄準點;
+        }
+        
+        
     }
 }
